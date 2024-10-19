7de13c6249d2df497d80727f94b77500640f2549
LopezWellsSebastian <105455846+LopezWellsSebastian@users.noreply.github.com>
2024-10-18 17:48:04 -0700

    popup
    Falta unos detalles pero para que avancen la seleccion de mapas y personajes

--- ludo_v2/Interfaz/Interfaz_Codigo/Menu.gd
+++ ludo_v2/Interfaz/Interfaz_Codigo/Menu.gd
@@ -1,5 +1,6 @@
-extends Control
-
+extends Control 
+@onready var menu = $Menu
+@onready var popup = $Emergente
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,9 +14,15 @@ func _process(delta: float) -> void:
 
 func _on_btn_jugar_pressed() -> void:
 	#get_tree().change_scene_to_file("res://Mapas/MapaIsleño/tumba_del_heroe.tscn")
-	get_tree().change_scene_to_file("res://Interfaz/Interfaz_Escenas/tipo_conexion_popUp.tscn")
-	DisplayServer.window_set_size(Vector2(942, 734))
+	#get_tree().change_scene_to_file("res://Interfaz/Interfaz_Escenas/tipo_conexion_popUp.tscn")
+	#DisplayServer.window_set_size(Vector2(942, 734))
+	popup.show()
 	
 
 func _on_btn_salir_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_emergente_close_requested() -> void:
+	popup.hide()
