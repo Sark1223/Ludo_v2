a971001373c34a04c12898cc9115a912306b1ac3
Karla Judith Santos Rivera <105814864+Sark1223@users.noreply.github.com>
2024-10-29 14:51:27 -0700

    codigito

---
+++ ludo_v2/Tableros/Escenas_tablero/mapa_1_movimiento_players.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+var posiciones = [
+	Vector2(-16,0), Vector2(-13, 0), Vector2(-10, 0), Vector2(-07, 0), Vector2(-04,0), Vector2(-04, -02),
+	Vector2(-4, -5), Vector2(-04,-08), Vector2(-04,-011), Vector2(-04, -14), Vector2(-1, -14), Vector2(2, -14), Vector2(2, -13),Vector2(2,-10), Vector2(20,-7), Vector2(20,-11),Vector2(20,-15),
+	Vector2(17,-16), Vector2(13,-16),Vector2(9,-16), Vector2(5,-16), Vector2(1,-16), Vector2(-3,-16), Vector2(-7,-16), Vector2(-11,-16), Vector2(-15,-16),
+	Vector2(-18,-15), Vector2(-18,-11),Vector2(-18,-7),Vector2(-18,-3),Vector2(-18,1),Vector2(-18,5),Vector2(-18,9),Vector2(-18,13),Vector2(-18,17),
+	Vector2(-15,19), Vector2(-11,19),Vector2(-7,19),Vector2(-3,19), Vector2(1,19), Vector2(1,15), Vector2(1,11), Vector2(1,7),	Vector2(1,1)
+]
