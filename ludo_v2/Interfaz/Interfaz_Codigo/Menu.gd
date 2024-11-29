62f39791a43716f0762b88f5f5e30469f7caccde
LopezWellsSebastian <105455846+LopezWellsSebastian@users.noreply.github.com>
2024-11-28 19:57:22 -0800

    Correccion de Menu Codigo

--- ludo_v2/Interfaz/Interfaz_Codigo/Menu.gd
+++ ludo_v2/Interfaz/Interfaz_Codigo/Menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	DisplayServer.window_set_size(Vector2(1050,600))
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,11 +13,9 @@ func _ready() -> void:
 
 
 func _on_btn_jugar_pressed() -> void:
-	#get_tree().change_scene_to_file("res://Mapas/MapaIsleño/tumba_del_heroe.tscn")
-	#get_tree().change_scene_to_file("res://Interfaz/Interfaz_Escenas/tipo_conexion_popUp.tscn")
-	#DisplayServer.window_set_size(Vector2(942, 734))
-	popup.show()
 	$Timer.start()
+	popup.show()
+	
 	
 
 func _on_btn_salir_pressed() -> void:
@@ -25,15 +23,19 @@ func _on_btn_salir_pressed() -> void:
 
 
 func _on_timer_timeout() -> void:
-	popup.hide()
+	popup.hide() 
 
 
 func _on_btn_online_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_btn_local_pressed() -> void: 
-
+func _on_btn_local_pressed() -> void:
+	
 	var scene = load("res://Interfaz/Interfaz_Escenas/multi_control.tscn").instantiate()
 	get_tree().root.add_child(scene) 
 	#get_tree().change_scene_to_file(scene)
+
+
+func _on_emergente_close_requested() -> void:
+	popup.hide()
